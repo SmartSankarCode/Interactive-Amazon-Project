47c638236a914d50ac9c755266627f056ebdfc98
sankarrayi <rayisankar8888@gmail.com>
2025-04-19 17:57:57 +0530

    Save data and generate HTML for delivery options

---
+++ javascript-amazon-project/data/deliveryOptions.js
@@ -0,0 +1,13 @@
+export const deliveryOptions = [{
+    id: '1',
+    deliveryDays: 7,
+    priceRupees: 0
+}, {
+    id: '2',
+    deliveryDays: 3,
+    priceRupees: 49  
+}, {
+    id: '3',
+    deliveryDays: 1,
+    priceRupees: 99
+}]
