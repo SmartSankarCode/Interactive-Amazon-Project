b2d91f262602b14f8832f47ba4f173d490586e38
sankarrayi <rayisankar8888@gmail.com>
2025-04-25 17:43:48 +0530

    convert code to promises

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -5,9 +5,25 @@ import {loadProducts} from '../data/products.js';
 // import '../data/cart-oop.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
+
+
+/* this is callbacks
 loadProducts(() => {
     renderCheckoutHeader();
     renderOrderSummary();
     renderPaymentSummary();
 });
+*/
 
+// callbacks make our code more nesting
+// promises keep our code more flat
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve();
+    });
+
+}).then(() => {
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+})
